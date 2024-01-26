1494e46bf2cd010ecd4f0e6bff8e81868f8b5c50
Andrie-E <stpaul.martinla@gmail.com>
2024-01-26 22:01:12 +0800

    pseudocode

--- function.py
+++ function.py
@@ -1,1 +1,7 @@
 # Write a function-called-exponent-base-exp-that-returns-an-int-value-of-base-raises-to-the-power-of-exp
+
+# Pseudocode
+
+# Function
+# Multiply base by itself exp times
+# Print result
